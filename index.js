0f05869b10d763f7f5e5a92d2f71b7836ed947c8
Naman <chauhannaman242@gmail.com>
2023-12-07 22:43:33 +0530

    Added delete button

--- index.js
+++ index.js
@@ -3,14 +3,30 @@
     const inputEl = document.getElementById("input-el");
     const inputBtn = document.getElementById("input-btn");
     const ulEl = document.getElementById("ul-el");
+    const deleteBtn = document.getElementById("delete-btn");
+
+    const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+    
+    if (leadsFromLocalStorage){
+        myLeads = leadsFromLocalStorage;
+        renderLeads();
+    }
+
+    deleteBtn.addEventListener("dblclick", function(){
+        localStorage.clear();
+        console.log("double clicked");
+        myLeads = [];
+        renderLeads();
+    })
+    
+
+
 
     inputBtn.addEventListener("click", function(){
         myLeads.push(inputEl.value);
         inputEl.value = "";
         localStorage.setItem("myLeads", JSON.stringify(myLeads));
         renderLeads();
-        
-        console.log(localStorage.getItem("myLeads"))
     })
 
     function renderLeads(){
